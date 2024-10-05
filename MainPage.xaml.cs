4d723bbab3def92bddec6f2788ff9edc090f9bb8
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-10-05 09:45:28 +0200

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_GoogleMaps_SimpleMap
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
